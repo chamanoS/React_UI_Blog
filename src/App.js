d9180944e6eb8fb21183249bc3b48b5374e7d98a
Chamano Ramashia <chamano.sr@gmail.com>
2022-01-17 09:16:17 +0200

    fetching data from backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import Topbar from "./components/topbar/Topbar";
-import Homepage from "./pages/homepage/Homepage";
+import Topbar from "./components/topbar/TopBar"
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
-  const currentUser = false;
+  const { user} = useContext(Context)
   return (
     <Router>
       <Topbar />
       <Routes>
-        <Route exact path="/" element={ <Homepage />} />
-        <Route exact path="/posts" element={ <Homepage />} />
-        <Route path="/register" element={currentUser ? <Homepage /> : <Register />} />
-        <Route path="/login" element={currentUser ? <Homepage /> : <Login />} />
-        <Route path="/post/:id" element={<Single />}/>
-        <Route path="/write" element={currentUser ? <Write /> : <Login />} />
-        <Route path="/settings" element={currentUser ? <Settings /> : <Login />} />
+        <Route exact path="/" element={ <Home />} />
+        <Route exact path="/posts" element={ <Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/post/:postId" element={<Single />}/>
+        <Route path="/write" element={user ? <Write /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Login />} />
       </Routes>
     </Router>
   );
